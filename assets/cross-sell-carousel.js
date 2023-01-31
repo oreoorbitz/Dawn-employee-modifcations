42c8e27b1fe2dc9c06fcaa1dd9c4f69216edbc8d
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-01-31 13:39:48 -0600

    added specification & mobile-first css

--- assets/cross-sell-carousel.js
+++ assets/cross-sell-carousel.js
@@ -1,20 +1,28 @@
 class CrossSellCarousel extends HTMLElement {
   constructor() {
     super();
-    this.glideInstance = new Glide('[data-glide]', {
+    this.glideInstance = new Glide('[data-glide-cross-sell]', {
       type: 'carousel',
       startAt: 0,
       perView: 2,
       gap: 15,
       breakpoints: {
+        1090: {
+          perView: 1
+        },
+        990: {
+          perView: 2
+        },
+        815: {
+          perView: 1
+        },
         749: {
-          perView: 3,
-          gap: 20
+          perView: 3
         },
         480: {
           perView: 2
         },
-        305: {
+        345: {
           perView: 1
         }
       }
