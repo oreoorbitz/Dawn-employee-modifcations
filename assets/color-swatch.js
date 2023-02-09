f40304f0721db21e696f1d719f7d36a914317681
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-02-09 15:23:35 -0600

    removed curly braces & added color variable

--- assets/color-swatch.js
+++ assets/color-swatch.js
@@ -1,14 +1,10 @@
 const colorSwatches = document.querySelectorAll('[data-color-swatch]')
 
-const toggleVisibilityOfSwatchName = (element) => {
+const toggleVisibilityOfSwatchName = (element) =>
   element.querySelector('[data-color-swatch-name]').classList.toggle('visually-hidden')
-}
-
-colorSwatches.forEach(swatch => {
-  swatch.addEventListener('mouseover', (event) => {
-    toggleVisibilityOfSwatchName(event.target)
-  })
-  swatch.addEventListener('mouseout', (event) => {
-    toggleVisibilityOfSwatchName(event.target)
-  })
+  colorSwatches.forEach(swatch => {
+    swatch.addEventListener('mouseover', (event) =>
+      toggleVisibilityOfSwatchName(event.target))
+    swatch.addEventListener('mouseout', (event) =>
+      toggleVisibilityOfSwatchName(event.target))
 })
