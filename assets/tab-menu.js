48a0206d37d26e534b979b3f86e0890deeb5103a
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-02-17 15:51:30 -0600

    tab menu function and style complete

--- assets/tab-menu.js
+++ assets/tab-menu.js
@@ -1,22 +1,33 @@
-const tabMenuListElements = document.querySelectorAll('[data-tm-list-item]')
-const tabMenuLinkElements = document.querySelectorAll('[data-tm-link]')
-const tabMenuContent = document.querySelectorAll('[data-tm-content]')
-const tabMenuList = document.querySelector('[data-tm-list]')
-const linksArray = [...tabMenuList.querySelectorAll('[data-tm-link]')]
-const link = 'data-tm-link'
-const listItem = '[data-tm-list-item]'
-const leftArrow = 'ArrowLeft'
-const upArrow = 'ArrowUp'
-const rightArrow = 'ArrowRight'
-const downArrow = 'ArrowDown'
+const tabs = document.querySelectorAll('[data-tm-link]')
+const tabpanels = document.querySelectorAll('[data-tm-content]')
+const tabpanelsArray = [...tabpanels]
+
+const keyPressIs = (keyCode, event) => event.code === keyCode 
+
+const arrowKeyPressIs = (keyCodeOne, keyCodeTwo, event) => 
+(event.key || event.code) === keyCodeOne || 
+(event.key || event.code) === keyCodeTwo
+
+const focusOnFocusableElementsInTabpanel = () => {
+  tabpanelsArray.forEach(tabpanel => {
+    const focusableElements = tabpanel.querySelectorAll(
+      'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'
+    ).length
+    focusableElements ? tabpanel.setAttribute("tabindex", "-1") : tabpanel.setAttribute("tabindex", "0")
+  })
+}
+
+const contentRelatedToLink = (tab) => {
+  const idOfPanel = `#${tab.dataset.tmLink}`
+  const relatedPanel = document.querySelector(`${idOfPanel}`)
+  return relatedPanel
+}
 
 const activateFirstPanel = (tabs, tabpanels) => {
   tabs[0].setAttribute('tabindex', '0')
   tabs[0].setAttribute('aria-selected', 'true')
   tabpanels[0].classList.add('visible')
 }
-
-activateFirstPanel(tabMenuLinkElements, tabMenuContent)
 
 const setSelectedTab = (element, tabs) => {
   const selectedId = element.id
@@ -32,32 +43,6 @@ const setSelectedTab = (element, tabs) => {
   })
 }
 
-const arrowKeyPressIs = (keyCodeOne, keyCodeTwo, event) => (event.key || event.code) === keyCodeOne || (event.key || event.code) === keyCodeTwo
-
-const createArrowNavigation = (links) => {
-  if (links.length > 1) {
-    const firstTab = links[0]
-    const lastTab = links[links.length - 1]
-
-    links.forEach(link => {
-      link.addEventListener('keydown', (event) => {
-        event.preventDefault()
-        if (arrowKeyPressIs(leftArrow, upArrow, event)) {
-          const indexOfPrevious = links.indexOf(link) - 1
-          link === firstTab ? links[links.length - 1].focus() : links[indexOfPrevious].focus()
-        } else if (arrowKeyPressIs(rightArrow, downArrow, event)) {
-          const indexOfNext = links.indexOf(link) + 1
-          link === lastTab ? links[0].focus() : links[indexOfNext].focus()
-        } else {
-          return
-        }
-      })
-    })
-  }
-}
-
-createArrowNavigation(linksArray)
-
 const removeClassFromAll = (elements, elementClass) => {
   elements.forEach(element => {
     element.classList.remove(elementClass)
@@ -65,50 +50,55 @@ const removeClassFromAll = (elements, elementClass) => {
 }
 const addClass = (element, classToAdd) => element.classList.add(classToAdd)
 
-const changeAttributeValueForAll = (elements, attribute, value) => elements.forEach(element => {
-  element.setAttribute(attribute, value)
-})
-
-const changeAttributeValue = (element, attribute, value) => element.setAttribute(attribute, value)
-
-const contentRelatedToLink = (linkElement) => {
-  const idOfPanel = `#${linkElement.dataset.tmLink}`
-  if (linkElement.closest('[data-tm-list]').nextElementSibling.hasAttribute('data-tm-content-container')) {
-    return linkElement.closest('[data-tm-list]').nextElementSibling.querySelector(idOfPanel)
+const handleArrowPressOfTab = (tabs, event) => {
+  const firstTab = tabs[0]
+  const lastTab = tabs[tabs.length - 1]
+  if (arrowKeyPressIs('ArrowLeft', 'ArrowUp', event)) {
+    const indexOfPrevious = tabs.indexOf(event.target) - 1
+    event.target === firstTab ? tabs[tabs.length - 1].focus() : tabs[indexOfPrevious].focus()
+  } else if (arrowKeyPressIs('ArrowRight', 'ArrowDown', event)) {
+    const indexOfNext = tabs.indexOf(event.target) + 1
+    event.target === lastTab ? tabs[0].focus() : tabs[indexOfNext].focus()
+  } else {
+    return
   }
 }
 
-
-const onClickOfTabLink = (linkElements, siblingElements, element, siblingElementClass, event) => {
-    removeClassFromAll(siblingElements, siblingElementClass)
-    const contentElement = contentRelatedToLink(element)
-    if (event.target.hasAttribute('data-tm-link')) {
-      addClass(contentElement, siblingElementClass)
-      setSelectedTab(event.target, linkElements)
-    }
-  }
-
-tabMenuLinkElements.forEach(link => {
-  link.addEventListener('click', () => { onClickOfTabLink(tabMenuLinkElements, tabMenuContent, link, 'visible', event) })
-})
-
-
-
-tabMenuLinkElements.forEach(link => {
-  link.addEventListener("keydown", (event) => {
-    if ((event.keyCode || event.which) === 32) {
-      setSelectedTab(link);
-      link.click();
-    }
-  });
-});
-
-
+const onSelectionOfTab = (tabs, tabpanels, event) => {
+  removeClassFromAll(tabpanels, 'visible')
+  const contentElement = contentRelatedToLink(event.target)
+  addClass(contentElement, 'visible')
+  setSelectedTab(event.target, tabs)
+}
 
 class TabMenu extends HTMLElement {
   constructor() {
     super();
+    
+    this.tabsArray = [...tabs]
+    this.tabpanelsArray = [...tabpanels]
 
+    activateFirstPanel(this.tabsArray, this.tabpanelsArray)
+
+    this.addEventListener('click', this.tabClickHandler.bind(this))
+    this.addEventListener('keydown', this.tabKeydownHandler.bind(this))
+  }
+  tabClickHandler (event) {
+    if (event.target.hasAttribute('data-tm-link')) {
+      onSelectionOfTab(this.tabsArray, this.tabpanelsArray, event)
+    }
+  }
+  tabKeydownHandler (event) {
+    if (event.target.hasAttribute('data-tm-link')) {
+      event.preventDefault()
+      if (keyPressIs('Enter', event) || keyPressIs('Space', event)) {
+        onSelectionOfTab(this.tabsArray, this.tabpanelsArray, event)
+      } else if (keyPressIs('Tab', event)) {
+        contentRelatedToLink(event.target).focus()
+      } else if (this.tabsArray.length > 1) {
+        handleArrowPressOfTab(this.tabsArray, event)
+      }
+    }
   }
 }
 
